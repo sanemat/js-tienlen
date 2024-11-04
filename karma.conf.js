902a837b66e65f66c344a35430b44843f2a7398c
Matt Murahashi Kenichi <o.gata.ken@gmail.com>
2024-11-04 13:11:39 +0000

    add settings

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: [],
+        files: [
+            'public/js/*.js',
+            'test/*.test.js'
+        ],
+        exclude: [],
+        browsers: ['ChromeHeadless'],
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: false,
+        concurrency: Infinity
+    });
+};
